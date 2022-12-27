68948e03f65b2676023129746d7c25ba01d7f4e3
VictorKmin <kminvictor@gmail.com>
2022-12-27 11:47:01 +0200

    Add testData for every single test case

--- test/app.test.js
+++ test/app.test.js
@@ -4,20 +4,22 @@ const three = require('../three');
 const four = require('../four');
 const five = require('../five');
 
-const testData = [
-  { input: [], output: 0 },
-  { input: [2, 0, 1, 5, 2, 7], output: 6 },
-  { input: [2, 1, 5, 2, 7, 4, 10], output: 7 },
-  { input: [4, 1, 3], output: 2 },
-  { input: [4, 0, 1, 3], output: 5 },
-  { input: [2, 4, 2], output: 0 },
-  { input: [7, 4], output: 0 },
-  { input: [7, 4, 3, 8, 14, 9, 0, 4, 20], output: 36 }
-];
-
 describe('Test yacht', () => {
 
+  // I have testData for every single test because some function can change input array
+
   test('Test function ONE', () => {
+    const testData = [
+      { input: [], output: 0 },
+      { input: [2, 0, 1, 5, 2, 7], output: 6 },
+      { input: [2, 1, 5, 2, 7, 4, 10], output: 7 },
+      { input: [4, 1, 3], output: 2 },
+      { input: [4, 0, 1, 3], output: 5 },
+      { input: [2, 4, 2], output: 0 },
+      { input: [7, 4], output: 0 },
+      { input: [7, 4, 3, 8, 14, 9, 0, 4, 20], output: 36 }
+    ];
+
     for (const { input, output } of testData) {
       const result = one(input);
 
@@ -26,6 +28,17 @@ describe('Test yacht', () => {
   });
 
   test('Test function TWO', () => {
+    const testData = [
+      { input: [], output: 0 },
+      { input: [2, 0, 1, 5, 2, 7], output: 6 },
+      { input: [2, 1, 5, 2, 7, 4, 10], output: 7 },
+      { input: [4, 1, 3], output: 2 },
+      { input: [4, 0, 1, 3], output: 5 },
+      { input: [2, 4, 2], output: 0 },
+      { input: [7, 4], output: 0 },
+      { input: [7, 4, 3, 8, 14, 9, 0, 4, 20], output: 36 }
+    ];
+
     for (const { input, output } of testData) {
       const result = two(input);
 
@@ -34,6 +47,17 @@ describe('Test yacht', () => {
   });
 
   test('Test function THREE', () => {
+    const testData = [
+      { input: [], output: 0 },
+      { input: [2, 0, 1, 5, 2, 7], output: 6 },
+      { input: [2, 1, 5, 2, 7, 4, 10], output: 7 },
+      { input: [4, 1, 3], output: 2 },
+      { input: [4, 0, 1, 3], output: 5 },
+      { input: [2, 4, 2], output: 0 },
+      { input: [7, 4], output: 0 },
+      { input: [7, 4, 3, 8, 14, 9, 0, 4, 20], output: 36 }
+    ];
+
     for (const { input, output } of testData) {
       const result = three(input);
 
@@ -42,6 +66,17 @@ describe('Test yacht', () => {
   });
 
   test('Test function FOUR', () => {
+    const testData = [
+      { input: [], output: 0 },
+      { input: [2, 0, 1, 5, 2, 7], output: 6 },
+      { input: [2, 1, 5, 2, 7, 4, 10], output: 7 },
+      { input: [4, 1, 3], output: 2 },
+      { input: [4, 0, 1, 3], output: 5 },
+      { input: [2, 4, 2], output: 0 },
+      { input: [7, 4], output: 0 },
+      { input: [7, 4, 3, 8, 14, 9, 0, 4, 20], output: 36 }
+    ];
+
     for (const { input, output } of testData) {
       const result = four(input);
 
@@ -50,6 +85,17 @@ describe('Test yacht', () => {
   });
 
   test('Test function FIVE', () => {
+    const testData = [
+      { input: [], output: 0 },
+      { input: [2, 0, 1, 5, 2, 7], output: 6 },
+      { input: [2, 1, 5, 2, 7, 4, 10], output: 7 },
+      { input: [4, 1, 3], output: 2 },
+      { input: [4, 0, 1, 3], output: 5 },
+      { input: [2, 4, 2], output: 0 },
+      { input: [7, 4], output: 0 },
+      { input: [7, 4, 3, 8, 14, 9, 0, 4, 20], output: 36 }
+    ];
+
     for (const { input, output } of testData) {
       const result = five(input);
 
